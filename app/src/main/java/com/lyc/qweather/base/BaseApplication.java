c93e859ae91f94e2c9978ec3567fd9f15836f4e5
wayne <rooney0928@hotmail.com>
2016-12-27 18:51:26 +0800

    create db

---
+++ app/src/main/java/com/lyc/qweather/base/BaseApplication.java
@@ -0,0 +1,12 @@
+package com.lyc.qweather.base;
+
+import org.litepal.LitePalApplication;
+
+/**
+ * Created by wayne on 2016/12/27.
+ */
+
+public class BaseApplication extends LitePalApplication{
+
+
+}
---
+++ app/src/main/java/com/lyc/qweather/db/City.java
@@ -0,0 +1,37 @@
+package com.lyc.qweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by wayne on 2016/12/27.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+}
